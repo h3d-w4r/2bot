c7fca3958088a30711010e3ffb5fb4dc8d2f8015
h3d-w4r <60212110+h3d-w4r@users.noreply.github.com>
2020-03-31 02:25:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ from colorama import Fore, init as color_ama
 from bs4 import BeautifulSoup
 
 os.system('cls' if os.name=='nt' else 'clear')
-
-# Get your own values from my.telegram.org
 api_id = 800812
 api_hash = 'db55ad67a98df35667ca788b97f771f5'
 
@@ -38,8 +36,6 @@ Options:
 5. Bitcoinclick_bot
 # '''
 dogeclick_channel = 'Dogecoin_click_bot'
-ltcclick_channel = 'Litecoin_click_bot'
-bchclick_channel = 'BCH_clickbot'
 zcclick_channel = 'Zcash_click_bot'
 
 def print_msg_time(message):
@@ -69,59 +65,123 @@ async def main():
 		exit(1)
 		
 	phone_number = sys.argv[1]
-		if not os.path.exists("session"):
-	    os.mkdir("session")
+	if not os.path.exists("session"):
+		os.mkdir("session")
 	client = TelegramClient('session/' + phone_number, api_id, api_hash)
 	await client.start(phone_number)
 	me = await client.get_me()
-f'NAMA AKUN : {me.first_name}({me.username})')
- 
-  #ZCASH_click_bot_VISIT
-	# Start command /visit
-	await client.send_message(zcclick_channel, '/visit')
-	
-	# Start visiting the ads
-	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
-	async def visit_ads(event):
-	
-	
-		original_update = event.original_update
-		if type(original_update)is not UpdateShortMessage:
-			if hasattr(original_update.message,'reply_markup') and type(original_update.message.reply_markup) is ReplyInlineMarkup:
-				url = event.original_update.message.reply_markup.rows[0].buttons[0].url
-				#url = event.message.reply_markup.rows[0].buttons[0].url
-			
-				if url is not None:
-					print_msg_time('SEDANG MASUK KE WEBSITE...')
+	print(Fore.CYAN+'['+Fore.YELLOW+' Telegram '+Fore.CYAN+']'+Fore.BLUE+f'NAMA AKUN	: {me.first_name}({me.username})')
+	print(Fore.CYAN+'['+Fore.YELLOW+' Telegram '+Fore.CYAN+']'+Fore.BLUE+f'NOMOR HP		: ({phone_number})\n'
 
-					# Parse the html of url
-					(status_code, text_response) = get_response(url)
-					parse_data = BeautifulSoup(text_response, 'html.parser')
-					captcha = parse_data.find('div', {'class':'g-recaptcha'})
-					
-					# Captcha detected
-					if captcha is not None:
-						print_msg_time(Fore.RED + 'CAPTCHA TERDETEKSI !!!'+ Fore.RED +' SKIP CAPTCHA...')
-									
-						# Clicks the skip
-						await client(GetBotCallbackAnswerRequest(
-							peer=zcclick_channel,
-							msg_id=event.message.id,
-							data=event.message.reply_markup.rows[0].buttons[1].data
-						))		
-		
-	# Print earned money
+	await client.send_message(zcclick_channel, '/join')
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def join_start(event):
+		message = event.raw_text
+		if 'You must join' in message:	
+			channel_name = re.search(r'You must join @(.*?) to earn', message).group(1)
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f' BERGABUNG KE @{channel_name}...' + Fore.RESET)
+			await client(JoinChannelRequest(channel_name))
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f' Sedang Loading ...')
+			time.sleep(2)
+			await client(GetBotCallbackAnswerRequest(
+				peer=zcclick_channel,
+				msg_id=event.message.id,
+				data=event.message.reply_markup.rows[0].buttons[1].data
+			))
 	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
 	async def wait_hours(event):
 		message = event.raw_text
+		if 'You must stay' in message:	
+			waiting_hours = re.search(r'at least (.*?) to earn', message).group(1)
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f' Sukses Join !!! tunggu {waiting_hours} Untuk rewards' + Fore.RESET)
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def no_ads(event):
+		message = event.raw_text
+		if 'no new ads available' in message:	
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + ' Maaf, Tidak Ada Iklan Sekarang !!!' + Fore.RESET)
+			
+
+	await client.send_message(zcclick_channel, '/bots')
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def join_start(event):
+		message = event.raw_text
+		if 'Forward a message to' in message:	
+			channel_msg = event.original_update.message.reply_markup.rows[0].buttons[0].url
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f' URL @{channel_msg}' + Fore.RESET)
+			if '?' in channel_msg:
+				channel_name = re.search(r't.me\/(.*?)\?', channel_msg).group(1)
+			elif '?' not in channel_msg:
+				channel_name = re.search(r't.me\/(.*?)', channel_msg).group(1)
+	
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f' MessageBot @{channel_name}...' + Fore.RESET)
+			await client.send_message(channel_name, '/start')
+			
+			@client.on(events.NewMessage(chats=channel_name, incoming=True))
+			async def earned_amount(event):
+				msg_id = event.message.id,
+				await client.forward_messages(zcclick_channel, msg_id, channel_name)
+	
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def earned_amount(event):
+		message = event.raw_text
 		if 'You earned' in message:	
-			print_msg_time(Fore.GREEN + f'{message}'+ Fore.RESET)
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + event.raw_text + Fore.RESET)
 			# No more ads
 	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
 	async def no_ads(event):
 		message = event.raw_text
 		if 'no new ads available' in message:	
-			print_msg_time(Fore.RED + 'MAAF, IKLAN SUDAH HABIS !!!' + Fore.RESET)
+			print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.RED + 'Maaf, Iklan Habis !!!' + Fore.RESET)
+
+	await client.send_message(zcclick_channel, '/visit')
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def visit_ads(event):	
+		original_update = event.original_update
+		if type(original_update)is not UpdateShortMessage:
+			if hasattr(original_update.message,'reply_markup') and type(original_update.message.reply_markup) is ReplyInlineMarkup:
+				url = event.original_update.message.reply_markup.rows[0].buttons[0].url
+				if url is not None:
+					print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN +' Sedang Mengunjungi Iklan Web ...')
+					(status_code, text_response) = get_response(url)
+					parse_data = BeautifulSoup(text_response, 'html.parser')
+					captcha = parse_data.find('div', {'class':'g-recaptcha'})
+					tt=parse_data.find('div',{'id':'headbar'})
+					if captcha is not None:
+						print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.RED + 'Warning .... Ada CAPTCHA !!!'+ Fore.RED +'Iklan Di SKIP CAPTCHA...')
+						await client(GetBotCallbackAnswerRequest(
+							peer=zcclick_channel,
+							msg_id=event.message.id,
+							data=event.message.reply_markup.rows[0].buttons[1].data
+						))
+					elif status_code==200 and captcha is None and tt is not None:
+						setPhoneNumber=tt.get('data-code')
+						data_timer=tt.get('data-timer')
+						data_token=tt.get('data-token')
+						logResponse(int(data_timer))
+						requests.post('http://dogeclick.com/reward/',data={'code':setPhoneNumber,'token':data_token},headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"} ,allow_redirects=True) 
+					elif status_code==307 and captcha is None and tt is not None:
+						setPhoneNumber=tt.get('data-code')
+						data_timer=tt.get('data-timer')
+						data_token=tt.get('data-token')
+						logResponse(int(data_timer))
+						requests.post('http://dogeclick.com/reward/',data={'code':setPhoneNumber,'token':data_token},headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"} ,allow_redirects=True)
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def wait_second(event):
+		message = event.raw_text
+		if 'Please stay on' in message:
+			sec = re.findall( r'([\d.]*\d+)', message)
+			logResponse(int(sec[0]))
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def wait_hours(event):
+		message = event.raw_text
+		if 'You earned' in message:	
+			print_msg_time(print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.GREEN + f'{message}'+ Fore.RESET)
+			# No more ads
+	@client.on(events.NewMessage(chats=zcclick_channel, incoming=True))
+	async def no_ads(event):
+		message = event.raw_text
+		if 'no new ads available' in message:	
+			print_msg_time(print_msg_time(Fore.YELLOW+'['+Fore.MAGENTA+' Bot_Zec '+Fore.YELLOW+']'+Fore.RED + 'Maaf, Ads Sedang Habis !!!' + Fore.RESET)
 		
 		exit(0)
 			
